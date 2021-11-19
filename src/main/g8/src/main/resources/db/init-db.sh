b8a82946f49afa331554f40ce398b3a3b1a3c8fe
Oliver Winks <oliver@paperplane.software>
2021-11-19 12:04:27 +0000

    escaped more $

--- src/main/g8/src/main/resources/db/init-db.sh
+++ src/main/g8/src/main/resources/db/init-db.sh
@@ -4,8 +4,8 @@ set -e
 POSTGRES="psql -U $name$_owner -d $name$"
 
 
-echo "Creating database role: $POSTGRES_WORKER"
+echo "Creating database role: \$POSTGRES_WORKER"
 
 $POSTGRES <<-EOSQL
-CREATE USER $name$_worker WITH LOGIN PASSWORD '$POSTGRES_WORKER_PASSWORD';
+CREATE USER $name$_worker WITH LOGIN PASSWORD '\$POSTGRES_WORKER_PASSWORD';
 EOSQL
